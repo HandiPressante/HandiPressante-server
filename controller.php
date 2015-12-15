e2c3725e01924330dd83f6a6f55fffd6231e3b6f
olivier-dj <olivierdejonckere@gmail.com>
2015-12-15 17:51:44 +0100

    changements

--- controller.php
+++ controller.php
@@ -1,9 +1,8 @@
 <?php
-require_once '../config.inc.php';
 
-function connect() {
-	global $config;
 
+function connect(){
+require 'config.inc.php';
 	$pdo = null;
 	try {
     	$pdo = new PDO('mysql:host=localhost;dbname=' . $config['database']['dbname'].';charset=UTF8', $config['database']['user'], $config['database']['pass']);
@@ -88,7 +87,7 @@ function getToilettes($x,$y,$rangx,$rangy){
 
 
 
-function getImages($id) {
+function getImages($id){
 	$db=connect();
     $succes=true;
 	$sql = "SELECT image FROM images  WHERE id_images = :id ";
@@ -111,6 +110,42 @@ function getImages($id) {
 	else
 		return null;
 }
+
+
+function lambert93ToWgs84($x, $y){
+	$x = number_format($x, 10, '.', '');
+	$y = number_format($y, 10, '.', '');
+	$b6  = 6378137.0000;
+	$b7  = 298.257222101;
+	$b8  = 1/$b7;
+	$b9  = 2*$b8-$b8*$b8;
+	$b10 = sqrt($b9);
+	$b13 = 3.000000000;
+	$b14 = 700000.0000;
+	$b15 = 12655612.0499;
+	$b16 = 0.7256077650532670;
+	$b17 = 11754255.426096;
+	$delx = $x - $b14;
+	$dely = $y - $b15;
+	$gamma = atan( -($delx) / $dely );
+	$r = sqrt(($delx*$delx)+($dely*$dely));
+	$latiso = log($b17/$r)/$b16;
+	$sinphiit0 = tanh($latiso+$b10*atanh($b10*sin(1)));
+	$sinphiit1 = tanh($latiso+$b10*atanh($b10*$sinphiit0));
+	$sinphiit2 = tanh($latiso+$b10*atanh($b10*$sinphiit1));
+	$sinphiit3 = tanh($latiso+$b10*atanh($b10*$sinphiit2));
+	$sinphiit4 = tanh($latiso+$b10*atanh($b10*$sinphiit3));
+	$sinphiit5 = tanh($latiso+$b10*atanh($b10*$sinphiit4));
+	$sinphiit6 = tanh($latiso+$b10*atanh($b10*$sinphiit5));
+	$longrad = $gamma/$b16+$b13/180*pi();
+	$latrad = asin($sinphiit6);
+	$long = ($longrad/pi()*180);
+	$lat  = ($latrad/pi()*180);
+	
+	return array('y84' => $lat,'x84' => $long);
+		
+}
+
 
 
 ?>
