dd3adc07662a37a9db9d7b94cf8eeb2cd7105cc5
nico <nleborgn@insa-rennes.fr>
2015-12-15 19:10:13 +0100

    No config in github

--- config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config['autoload'] = __DIR__ . '/vendor/autoload.php';
-
-$config['database']['dbname'] = 'handi'; 
-$config['database']['user'] = 'root';
-$config['database']['pass'] = 'azepoi';
-?>
