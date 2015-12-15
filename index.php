3e5112739c1d4f87faf3ddcce746cacf939db987
olivier-dj <olivierdejonckere@gmail.com>
2015-12-15 11:48:32 +0100

    mardi

--- index.php
+++ index.php
@@ -8,4 +8,13 @@ echo"<pre>";
 print_r($wc);
 echo "</pre>";
 
+$wc=getImages(80);
+echo"<pre>";
+print_r($wc);
+echo "</pre>";
+
+
+echo"<pre>";
+print_r(lambert93ToWgs84('352914.52','6785627.76'));
+echo "</pre>";
 ?>
