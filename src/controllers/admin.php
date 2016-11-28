dd60a2d1936c4a7031df6ce6aa75868ad8c61c47
Nico-lb <nicolb.dev@gmail.com>
2016-11-28 17:47:25 +0100

    New features : show admin list, add admin

--- src/controllers/admin.php
+++ src/controllers/admin.php
@@ -33,3 +33,54 @@ $app->get('/admin', function ($request, $response, $args) {
 
     return $this->renderer->render($response, 'admin.html.twig', array('user' => $user));
 });
+
+$app->get('/admin/manage-access', function ($request, $response, $args) {
+    $stmt = $this->pdo->prepare('SELECT id, email FROM admins ORDER BY id DESC');
+    $stmt->execute();
+    $admins = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+    return $this->renderer->render($response, 'manage-access.html.twig', array('admins' => $admins));
+});
+
+$app->get('/admin/add-access', function ($request, $response, $args) {
+    return $this->renderer->render($response, 'add-access.html.twig');
+});
+
+$app->post('/admin/add-access', function ($request, $response) {
+    $data = $request->getParsedBody();
+    $signup = [];
+    $signup['email'] = filter_var($data['email'], FILTER_SANITIZE_STRING);
+    $signup['password'] = filter_var($data['password'], FILTER_SANITIZE_STRING);
+    $signup['password-confirm'] = filter_var($data['password-confirm'], FILTER_SANITIZE_STRING);
+
+    $error = '';
+    if (filter_var($signup['email'], FILTER_VALIDATE_EMAIL)) {
+        if ($signup['password'] == $signup['password-confirm']) {
+            if (strlen($signup['password']) >= 8) {
+                $stmt = $this->pdo->prepare('SELECT COUNT(*) FROM admins WHERE email = :email');
+                $stmt->bindParam(':email', $signup['email']);
+                $stmt->execute();
+                $count = $stmt->fetchColumn();
+
+                if ($count == 0) {
+                    $stmt = $this->pdo->prepare('INSERT INTO admins (email, pass_hash) VALUES (:email, :pass_hash)');
+                    $stmt->bindParam(':email', $signup['email']);
+                    $stmt->bindParam(':pass_hash', password_hash($signup['password'], PASSWORD_BCRYPT));
+                    $stmt->execute();
+
+                    return $response->withRedirect("/admin/manage-access");
+                } else {
+                    $error = 'Cette adresse e-mail est déjà utilisée.';
+                }
+            } else {
+                $error = 'Le mot de passe doit contenir au moins 8 caractères.';
+            }
+        } else {
+            $error = 'Les mots de passe ne sont pas identiques.';
+        }
+    } else {
+        $error = 'Veuillez entrer une adresse e-mail valide.';
+    }
+
+    return $this->renderer->render($response, 'add-access.html.twig', array('error' => $error, 'email' => $signup['email']));
+});
