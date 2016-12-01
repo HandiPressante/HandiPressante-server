9d935ed40b82c132e8bff4c343589e459b4f56c9
Nico-lb <nicolb.dev@gmail.com>
2016-12-01 12:45:58 +0100

    Auto-redirection if already logged

--- src/controllers/admin.php
+++ src/controllers/admin.php
@@ -2,16 +2,25 @@
 require_once __DIR__ . '/../Auth/User.php';
 
 $app->get('/admin/login', function ($request, $response, $args) {
+    $user = new \Auth\User();
+    if ($user->isAuthenticated()) {
+        return $response->withRedirect("/admin");
+    }
+
     return $this->renderer->render($response, 'login.html.twig', $args);
 });
 
 $app->post('/admin/login', function ($request, $response) {
+    $user = new \Auth\User();
+    if ($user->isAuthenticated()) {
+        return $response->withRedirect("/admin");
+    }
+
 	$data = $request->getParsedBody();
     $login_data = [];
     $login_data['email'] = filter_var($data['email'], FILTER_SANITIZE_STRING);
     $login_data['password'] = filter_var($data['password'], FILTER_SANITIZE_STRING);
 
-    $user = new \Auth\User();
     $success = $user->authenticate($login_data['email'], $login_data['password'], $this->pdo);
 
     if ($success) {
