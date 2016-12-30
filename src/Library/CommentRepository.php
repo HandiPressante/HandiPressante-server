853c0fa500af047700731b19ffcb22e3cac0616b
Nico-lb <nicolb.dev@gmail.com>
2016-12-30 19:30:09 +0100

    Added CRUD features to CommentRepository

---
+++ src/Library/CommentRepository.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Library;
+
+class CommentRepository extends Repository {
+
+	public function getByToilet($toiletId) {
+		$stmt = $this->pdo->prepare('SELECT id, username, content, postdate FROM comments WHERE toilet_id = :toilet_id ORDER BY postdate DESC');
+		$stmt->bindParam(":toilet_id", $toiletId);
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	public function add($toiletId, $userId, $username, $content) {
+		$stmt = $this->pdo->prepare('INSERT INTO comments (toilet_id, user_id, username, content, postdate) VALUES (:toilet_id, :user_id, :username, :content, NOW())');
+		$stmt->bindParam(':toilet_id', $toiletId);
+		$stmt->bindParam(":user_id", $userId);
+		$stmt->bindParam(":username", $username);
+		$stmt->bindParam(":content", $content);
+		return $stmt->execute();
+	}
+
+	public function update($commentId, $username, $content) {
+		$stmt = $this->pdo->prepare('UPDATE comments SET username = :username, content = :content WHERE id = :id');
+		$stmt->bindParam(':username', $username);
+		$stmt->bindParam(":content", $content);
+		$stmt->bindParam(":id", $commentId);
+		return $stmt->execute();
+	}
+
+	public function remove($commentId) {
+		$stmt = $this->pdo->prepare('DELETE FROM comments WHERE id = :id');
+		$stmt->bindParam(':id', $commentId);
+		$stmt->execute();
+	}
+};
