f13a4ae4612ba1a1cd4a2d999da4ecf9f8312771
Nico-lb <nicolb.dev@gmail.com>
2016-12-30 19:22:46 +0100

    Added save toilet function to API

--- src/Controller/ToiletController.php
+++ src/Controller/ToiletController.php
@@ -63,6 +63,61 @@ class ToiletController extends Controller {
 		return $this->ci->json->render($response, $apiResponse->toArray());
 	}
 
+	public function save($request, $response) {
+		$data = $request->getParsedBody();
+
+		if (!isset($data['uuid']) || 
+			!isset($data['toilet_name']) ||
+			!isset($data['toilet_adapted']) ||
+			!isset($data['toilet_charged']) ||
+			!isset($data['toilet_description']) ||
+			!isset($data['toilet_latitude']) ||
+			!isset($data['toilet_longitude']))
+		{
+			$apiResponse = new ApiErrorResponse('Requête invalide.');
+			return $this->ci->json->render($response, $apiResponse->toArray());
+		}
+
+		$uuid = strtolower(filter_var($data['uuid'], FILTER_SANITIZE_STRING));
+		
+		$name = filter_var($data['toilet_name'], FILTER_SANITIZE_STRING);
+		$adapted = (bool) $data['toilet_adapted'];
+		$charged = (bool) $data['toilet_charged'];
+		$description = filter_var($data['toilet_description'], FILTER_SANITIZE_STRING);
+		$latitude = (double) $data['toilet_latitude'];
+		$longitude = (double) $data['toilet_longitude'];
+
+		if (empty($uuid) || // todo : check uuid format with regexp
+			empty($name) || (strlen($name) > 40) ||
+			!$this->validateLatLong($latitude, $longitude))
+		{
+			$apiResponse = new ApiErrorResponse('Requête invalide.');
+			return $this->ci->json->render($response, $apiResponse->toArray());
+		}
+
+		$repo = $this->getRepository('Toilet');
+		$success = false;
+
+		$update = isset($data['toilet_id']) && ((int)$data['toilet_id'] > 0);
+		if ($update)
+		{
+			$id = (int) $data['toilet_id'];
+			$success = $repo->update($id, $name, $adapted, $charged, $description, $latitude, $longitude);
+		}
+		else
+		{
+			$success = $repo->add($name, $adapted, $charged, $description, $latitude, $longitude, $uuid);
+		}
+
+		if ($success) {
+			$apiResponse = new ApiSuccessResponse();
+		} else {
+			$apiResponse = new ApiErrorResponse('Enregistrement impossible.');
+		}
+
+		return $this->ci->json->render($response, $apiResponse->toArray());
+	}
+
 	private function validateLatLong($lat, $long) {
 		return ($lat >= -90) && ($lat <= 90) && ($long >= -180) && ($long <= 180);
 	}
