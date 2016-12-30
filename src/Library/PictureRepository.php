7550c046c8219bc4898b3285879121bb824f7576
Nico-lb <nicolb.dev@gmail.com>
2016-12-30 19:32:13 +0100

    Added CRUD features to PictureRepository

---
+++ src/Library/PictureRepository.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Library;
+
+class PictureRepository extends Repository {
+
+	public function getByToilet($toiletId) {
+		$stmt = $this->pdo->prepare('SELECT id, toilet_id, filename, postdate FROM pictures WHERE toilet_id = :toilet_id ORDER BY postdate DESC');
+		$stmt->bindParam(":toilet_id", $toiletId);
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	public function add($toiletId, $filename) {
+		$stmt = $this->pdo->prepare('INSERT INTO pictures (toilet_id, filename, postdate) VALUES (:toilet_id, :filename, NOW())');
+		$stmt->bindParam(':toilet_id', $toiletId);
+		$stmt->bindParam(":filename", $filename);
+		return $stmt->execute();
+	}
+
+	public function remove($pictureId) {
+		$stmt = $this->pdo->prepare('DELETE FROM pictures WHERE id = :id');
+		$stmt->bindParam(':id', $pictureId);
+		$stmt->execute();
+	}
+};
