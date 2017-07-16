a1df95b9ce86f8149918da3bc85780b9191963a7
Nicolas Le Borgne <nicolb.dev@gmail.com>
2017-07-16 15:20:06 +0100

    Compatibility with online.net constraints

--- public/index.php
+++ public/index.php
@@ -9,22 +9,22 @@ if (PHP_SAPI == 'cli-server') {
     }
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/slim/vendor/autoload.php';
 
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require __DIR__ . '/slim/src/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+require __DIR__ . '/slim/src/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/../src/middleware.php';
+require __DIR__ . '/slim/src/middleware.php';
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
+require __DIR__ . '/slim/src/routes.php';
 
 // Run app
 $app->run();
